dcc93864ad1ae0971ee9d9ef5bb621a96be0e115
Victor Williams <61339705+classalto@users.noreply.github.com>
2021-02-18 18:59:04 -0800

    deployed

---
+++ index.js
@@ -0,0 +1,6 @@
+const {app} = require('./app.js');
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Okay, it's running now! And your proof is that you can see this in the terminal. Example app listening at http://localhost:${port}`);
+});
